fa6593473c89ace24d0ff639b282a011ef06e246
Lucie M. <luma8925@colorado.edu>
2026-04-29 20:40:06 -0600

    finalhw

--- FINALPROJECT/main.js
+++ FINALPROJECT/main.js
@@ -1,71 +1,83 @@
-let score = 0;
+let currentScore = 0;
 let duckX;
 let duckY;
 let duckSpeed = 7;
 let deerX;
 let deerY;
 let deerSpeed = 10;
-let backgroundImage;
 let deerButton;
 let duckButton;
 let gunshotAudio;
 let targetImage;
+let backgroundImage;
+let submitText = false;
+let message = "";
 
 function preload() {
-  backgroundImage = loadImage("background.jpeg");
   targetImage = loadImage("target.png");
   deerButton = loadImage("deer.png");
   duckButton = loadImage("duck.png");
   gunshotAudio = loadSound("gunshot.mp3");
+  backgroundImage = loadImage("background.png");
+  gameFont = loadFont("PressStart2P-Regular.ttf");
 }
 
 //GUN SHOT MECHANICS
 function mousePressed(){
 userStartAudio();
-gunshotAudio.currentTime = 0;
+gunshotAudio.stop();
 gunshotAudio.play();
 gunshotAudio.setVolume(0.2) 
   
 //SCORE MECHANICS
  if(
   mouseX > duckX &&
-  mouseX < duckX + 100 &&
+  mouseX < duckX + 150 &&
   mouseY > duckY &&
-  mouseY < duckY + 100
+  mouseY < duckY + 150
   ){
-    score +=1;
+    currentScore +=5;
   }
     if (
     mouseX > deerX &&
     mouseX < deerX + 200 &&
     mouseY > deerY &&
-    mouseY < deerY + 150
+    mouseY < deerY + 200
   ) {
-    score -= 1;
+    currentScore -= 5;
   }
+  currentScore=constrain(currentScore, 0, 100);
 }
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+canvasSize = createCanvas(800, 700);
 
-//DUCK STARTING POSITION + Y SPAWN RANGE
+//SUBMIT BUTTON FORMAT
+submitButton = document.getElementById("submitButton");
+submitButton.addEventListener("click", submit);
+textFont(gameFont);
+//DUCK STARTING POSITION
   duckX = -100;
-  duckY = random(50, 250);
+  duckY = random(25, 250);
   
-//DEER STARTING POSITION + Y SPAWN RANGE
+//DEER STARTING POSITION
   deerX = -100;
   deerY = random(height*0.5, height*0.8)
 }
 
 function draw() {
-    background(0);
-    image(backgroundImage, 0, 0, width, height);
-
+image(backgroundImage,0,0,width,height);
+//SUBMIT MESSAGE FORMAT
+if (submitText) {
+    textAlign(CENTER);
+    textSize(40);
+    text(message, width/2, height/1.5);
+}
   // DUCK MECHANICS
   duckX += duckSpeed;
   if (duckX > width + 100) {
     duckX = -100;
-    duckY = random(35, 100);
+    duckY = random(10, 250);
   }
   
   image(duckButton, duckX, duckY, (width * 0.15), (height * 0.15));
@@ -79,8 +91,19 @@ deerX += deerSpeed;
   image(deerButton, deerX, deerY, (width*0.25), (height*0.20));
 
   
-//SCOREBOARD MECHANICS
-  fill("white");
+//SCOREBOARD
+  fill("black");
   textSize(24);
-  text("VOLUME: " + score, 15, 40);
+  textAlign(CENTER);
+  text("VOLUME: " + currentScore, width/2, height/7);
+  
+}
+//SUBMIT MESSAGE 
+function submit () {
+message = "VOLUME SUBMITTED!";
+submitText = true;
+setTimeout(hideText, 3000);
+}
+function hideText () {
+    submitText = false;
 }
